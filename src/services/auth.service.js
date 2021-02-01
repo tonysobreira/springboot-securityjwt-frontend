dcb841dde9aca6c50dff4abf0da81f2f5a26afef
Tony Sobreira <tonysobreira@gmail.com>
2021-02-01 16:05:52 -0300

    test

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://springboot-securityjwt-backend.herokuapp.com/api/auth/";
+const API_URL = "https://springboot-securityjwt-backend.herokuapp.com/api/auth/";
 
 class AuthService {
     login(username, password) {
--- src/services/user.service.js
+++ src/services/user.service.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import authHeader from './auth-header';
 
-const API_URL = 'http://springboot-securityjwt-backend.herokuapp.com/api/test/';
+const API_URL = 'https://springboot-securityjwt-backend.herokuapp.com/api/test/';
 
 class UserService {
     getPublicContent() {
